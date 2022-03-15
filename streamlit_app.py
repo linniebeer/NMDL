3da50b1d52a9f8f935ae96e46f93e763b7ba20cd
linniebeer <101671481+linniebeer@users.noreply.github.com>
2022-03-15 21:40:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+def main():
+	st.title("File Upload Tutorial")
+
+	menu = ["Image","Dataset","DocumentFiles","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	if choice == "Image":
+		st.subheader("Image")
+
+    elif choice == "Dataset":
+		st.subheader("Dataset")
+
+    elif choice == "DocumentFiles":
+		st.subheader("DocumentFiles")
