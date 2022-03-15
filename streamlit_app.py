93e1a4aebe38c97ae42bb57e7171861cef47cf2c
linniebeer <101671481+linniebeer@users.noreply.github.com>
2022-03-15 21:51:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 def main():
-	st.title("File Upload Tutorial")
-
-	data_file = st.file_uploader("Upload xlsx",type=["xlsx"])
-
-        if data_file is not None:
-		file_details = {"filename":data_file.name, "filetype":data_file.type,
-                            "filesize":data_file.size}
-		
-		st.write(file_details)
-		df = pd.read_csv(data_file)
-		st.dataframe(df)
+    st.title("File uploader")
+    
+    data_file = st.file_uploader("Upload xlsx", type=["xlsx"])
+    
+    if data_file is not None:
+        file_details = {"filename": data_file.name, "filetype":data_file.type,
+                        "filesize":data_file.size}
+        
+        st.write(file_details)
+        df = pd.read_csv(data_file)
+        st.dataframe(df)
