e0cf569b9f6287ba904aad213101aa50e9de97ba
linniebeer <101671481+linniebeer@users.noreply.github.com>
2022-03-15 22:11:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,3 @@ if data_file is not None:
     file_details = {"filename": data_file.name, "filetype":data_file.type,
                     "filesize":data_file.size}
 st.write(file_details)
-df = pd.read_excel(data_file)
-st.dataframe(df)
